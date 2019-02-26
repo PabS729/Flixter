171e294ab2b056e05f98e1a5b875814485a52cc1
PabS729 <44881250+PabS729@users.noreply.github.com>
2019-02-25 21:03:46 -0800

    Add files via upload

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Minjing Shi on 2/25/19.
+//  Copyright © 2019 Minjing Shi. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
